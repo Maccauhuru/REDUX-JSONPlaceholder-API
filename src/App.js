8c9d9d22ec10aa50fac394faf908fe58ce2f8090
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-05-13 18:49:50 -0500

    imported components and modified header element for rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import'./App';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+// import Albums from './components/Albums';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PostForm />
+        <hr / >
+        <Posts />
       </div>
     );
   }
